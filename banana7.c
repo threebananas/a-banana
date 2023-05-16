1f69df0c4f6bf834c134d5cff972a30a7c85a8d2
threebananas <128569822+threebananas@users.noreply.github.com>
2023-05-16 06:25:08 +0000

    switch문

---
+++ banana7.c
@@ -0,0 +1,18 @@
+// #include <stdio.h>
+// main(){
+//   int x;
+//   printf("1번 사과, 2번 키위,3번 복숭아, 4번 수박");
+//   scanf("%d", &x);
+
+//   if(x==1)
+//     printf("사과를 좋아하는군요!");
+//   else if(x==2)
+//     printf("키위를 좋아하는군요!");
+//   else if(x==3)
+//     printf("복숭아를 좋아하는군요!");
+//   else if(x==4)
+//     printf("수박을 좋아하는군요!");
+//   else
+//     printf("당신은 아마도 과일을 좋아하지 않는 것 같군요");
+//   printf("좋아하는 과일 조사 끝");
+// }
