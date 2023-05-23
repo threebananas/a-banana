0abe10e9642b061aad322dc74bf02d289e204457
threebananas <128569822+threebananas@users.noreply.github.com>
2023-05-23 06:11:44 +0000

    구구단 만들기

---
+++ .ccls-cache/@home@runner@a-banana/banana11.c
@@ -0,0 +1,13 @@
+// #include <stdio.h>
+// main(){
+//         int i;
+//         int sum=0;
+
+//         i=1;
+
+//         while(i<=2019){
+//         sum +=i;
+//         i++;
+//         }
+//         printf("1에서 2019까지의 합은 %d\n", sum); 
+// }
